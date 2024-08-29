a624f3789233d48640fac1acfe13164f3c9e716e
ludmilasalema <salema.souza@escola.pr.gov.br>
2024-08-29 11:26:32 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background ('black')
+}
+
+function draw() {
+  
+  fill("pink")
+if(mouseIsPressed) {  rect (mouseX, mouseY, 20, 20);
+  stroke("black");
+}
+}
